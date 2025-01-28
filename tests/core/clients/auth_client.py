ce7742dbc2a1234c6391ebe6ffa1d6d330b057ca
Евгений Сахарук <e.sakharuk@sbereducation.ru>
2025-01-28 12:18:05 +0300

    first commit

--- tests/core/clients/auth_client.py
+++ tests/core/clients/auth_client.py
@@ -8,7 +8,6 @@ class AuthClient(BaseClient):
     def get_token(self, login):
         payload = {'login': accounts[login]['email'], 'password': accounts[login]['password']}
 
-
         response = requests.post(url=f'{self.base_url}/login', data=payload, headers=self.headers)
         print(f'\nResponse /login: ${response.__dict__}')
         assert response.status_code == 200, f'Status code {response.status_code} != 200'
